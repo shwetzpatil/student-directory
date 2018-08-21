11b2a1bbe11771c0c635e0d6a388383b7123f918
shwetzpatil <shwetzpatil@gmail.com>
2018-08-21 14:49:11 +0000

    8.1 serial number added

--- directory.rb
+++ directory.rb
@@ -17,8 +17,8 @@ def print_header
 end
 
 def print(names)
-  names.each do |name|
-    puts "#{name[:name]} (#{name[:cohort]} cohort)"
+  names.each_with_index do |name, index|
+    puts "#{index + 1}. #{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
 
