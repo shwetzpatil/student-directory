de8d8b290469a8f0f719aaa684fbc440d9901f89
shwetzpatil <shwetzpatil@gmail.com>
2018-08-21 11:40:46 +0000

    define methods

--- directory.rb
+++ directory.rb
@@ -1,22 +1,33 @@
 # list of stuents in array
 students = [
-    "Dr. Hannibal Lecter",
-    "Darth Vader",
-    "Nurse Ratched",
-    "Micheal Corleone",
-    "Alex DeLarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddy Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates"
-    ]
-# list of students
-puts "the students of Villain Academy"
-puts "--------------"
-students.each do |student|
-  puts student
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Micheal Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+  ]
+  
+def print_headr
+  puts "The students of Villain Academy"
+  puts "--------------"
 end
-# total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+# calling methods
+print_headr
+print(students)
+print_footer(students)
