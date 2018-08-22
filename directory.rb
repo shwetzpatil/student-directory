55336e2eb6f932eacf0e66638429c87a95c696bd
shwetzpatil <shwetzpatil@gmail.com>
2018-08-22 14:56:40 +0000

    8.12 only print list if there is at least one student

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@ def input_students
   puts "Student Directory".center(50,"*")
   students = []
   cohorts = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
-  puts "Please enter the names of the student"
+  puts "Please enter the details of the student"
   name = gets.delete!("\n").capitalize
  
   while !name.empty? do
@@ -36,36 +36,39 @@ def print_header
 end
 
 def print(names)
-  student_hash = {}
-  names.each do |student_with_cohort|
-    curr_cohort = student_with_cohort[:cohort] 
-    details = [student_with_cohort[:name], student_with_cohort[:age], student_with_cohort[:country]]
-    
-    if student_hash[curr_cohort] == nil
-      student_hash[curr_cohort] = [details]
-    else
-    student_hash[curr_cohort].push(details)
+  if names.length > 0
+    student_hash = {}
+    names.each do |student_with_cohort|
+      curr_cohort = student_with_cohort[:cohort] 
+      details = [student_with_cohort[:name], student_with_cohort[:age], student_with_cohort[:country]]
+      
+      if student_hash[curr_cohort] == nil
+        student_hash[curr_cohort] = [details]
+      else
+      student_hash[curr_cohort].push(details)
+      end
     end
-  end
-  # puts student_hash
-  student_hash.each do |key, value|
-    puts "#{key.upcase} Cohort".center(50, "*")
-    count = 1
-    value.each do |student|
-      puts "#{count}"
-      puts "Name : #{student[0]}"
-      puts "Country of Birth : #{student[1]}"
-      puts "Age : #{student[2]}"
-      count += 1
+    # puts student_hash
+    student_hash.each do |key, value|
+      puts "#{key.upcase} Cohort".center(50, "*")
+      count = 1
+      value.each do |student|
+        puts "#{count}"
+        puts "Name : #{student[0]}"
+        puts "Country of Birth : #{student[1]}"
+        puts "Age : #{student[2]}"
+        count += 1
+      end
     end
+  # else
+  #   puts "Sorry, empty list"
   end
-  
 end
 
 def print_footer(names)
   if names.count > 1
     puts "Overall, we have #{names.count} great students".center(50, "*")
-  else
+  elsif names.count == 1
     puts "Overall, we have #{names.count} great student".center(50, "*")
   end
 end
--- typo.rb
+++ typo.rb
@@ -23,8 +23,12 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
+  if students.length > 0
+    students.each do |student|
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  else
+    puts "Sorry, Empty List"
   end
 end
 
