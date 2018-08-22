0f23131fefedc98e35a15a4ecc08472438769c5d
shwetzpatil <shwetzpatil@gmail.com>
2018-08-22 09:34:36 +0000

    8.6 use of center method

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
   puts "To finish, just hit return twice"
-  puts "Please add details of the students"
+  puts "Student Directory".center(50,"*")
   students = []
   puts "Please enter the names of the student"
   name = gets.chomp
@@ -19,20 +19,22 @@ def input_students
 end
   
 def print_header
-  puts "The students of Villain Academy"
-  puts "--------------"
+  puts "The students of Villain Academy".center(50,"*")
+  puts "___________".center(50, "_")
 end
 
 def print(names)
   count = 0
   while count < names.length do
-    puts " #{count + 1}. #{names[count][:name]} (#{names[count][:cohort]}) cohort from #{names[count][:country]} of age #{names[count][:age]}."
+    puts " #{count + 1}. #{names[count][:name]} (#{names[count][:cohort]}) cohort".center(50," ")
+    puts "Country Of Birth : #{names[count][:country]}".center(50," ")
+    puts "Age : #{names[count][:age]}".center(50," ")
     count += 1
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(50, "*")
 end
 # calling methods
 students = input_students
