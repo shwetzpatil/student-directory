8a09ce34732d3ae916c5c3a20c796c5626baa627
shwetzpatil <shwetzpatil@gmail.com>
2018-08-23 09:19:28 +0000

    adds an interacive menu

--- directory.rb
+++ directory.rb
@@ -72,8 +72,35 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great student".center(50, "*")
   end
 end
+
+def interactive_menu
+  students = []
+  
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    
+    selection = gets.chomp
+    
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
 # calling methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+# students = input_students
+# print_header
+# print(students)
+# print_footer(students)
+interactive_menu
+
